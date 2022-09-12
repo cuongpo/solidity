c9cdeb17bb601742be15d3727b777bb751ebc712
cuongpham <61429471+cuongpo@users.noreply.github.com>
2022-09-12 10:19:33 +0700

    basic

---
+++ basic/simple-mapping.sol
@@ -0,0 +1,16 @@
+// "SPDX-License-Identifier: UNLICENSED"
+
+pragma solidity ^0.5.13;
+
+contract SimpleMapping {
+    mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
+
+    function setValue(uint _index) public {
+        myMapping[_index] = true;
+    }
+
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
+}
